920a9e969f53d2b1b68dd8ca9919ea5008dd7855
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-07-23 15:33:37 +0000

    Fix some mistakes in main.h file

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <limits.h>
-#include <stdio.h>
+
+#include <stdarg.h>
 #include <stdlib.h>
 
 /*_putchar.c -> Samy*/
@@ -17,19 +17,19 @@ int _printf(const char *format, ...);
 
 /*int2str.c -> Samy */
 char *int2str(char length, void *ptr); /* length {'\0' , 'l', 'h'} */
-char *uint2str(const unsinged int n);
+char *uint2str(const unsigned int n);
 
 /*handle_percentage.c -> Abdullrahmen*/
-char *handle_ percentage(char *str, const void* const ptr);
-char *handle_variable(char *str, const void* const ptr);
-char *handle_ precision(char *str);
+char *handle_percentage(char *str, void const * const ptr);
+char *handle_variable(char *str, void const * const ptr);
+char *handle_precision(char *str);
 char *handle_width(char *str);
 char *handle_flag(char *str);
 
 /*numerical_conversions.c -> Samy*/
-char *uint2bin(const unsigned int n);
-char *int2octal(char length, const void* const ptr);
-char *int2hex(char length, const void* const ptr);
+char *uint2bin(char length, void const * const ptr); /* length {'ul' (unsigned long), 'u' (unsigned int)} */
+char *int2octal(char length, void const * const ptr); /* length {'\0' , 'l', 'h'} */
+char *int2hex(char length, void const * const ptr); /* length {'\0' , 'l', 'h'} */
 char *custom_specifier_S(char *str);
 char *rot13(char *str);
 
