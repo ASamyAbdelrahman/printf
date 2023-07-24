893aaf414632ee47ef673afc17d3231b8e4b88fb
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-07-24 22:13:56 +0300

    C0nvert the data type of the number

---
+++ convert_data_type.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * convert2signed - converts the data type of a number
+ * @num: the required number
+ * @length: the required data type
+ */
+
+long int convert2signed(long int num, char length)
+{
+	if (length == 'l')
+		return (num);
+	else if (length == 'h')
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert2unsigned - converts the data type of a number
+ * @num: the required number
+ * @length: the required data type
+ */
+unsigned long int convert2unsigned(unsigned long int num, char length)
+{
+	if (length == 'l')
+		return (num);
+	else if (length == 'h')
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
