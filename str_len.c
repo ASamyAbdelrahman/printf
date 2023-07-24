a0ae4039ce481a9cc2925c1b20508657748aab7b
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-07-24 03:55:22 +0300

    The length of the string

---
+++ str_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * str_len - gets the length of the string.
+ * @str: The required string.
+ *
+ * Return: on success len
+ */
+unsigned int str_len(char *str)
+{
+	unsigned int len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
