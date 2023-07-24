6ce532243d866e724efe22d99c2f80704eccf858
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-07-25 02:02:55 +0300

    Converts integer to hexa

---
+++ int_hexa.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * int2hex - converts integer to hexa
+ * @length: the required data type
+ * @ptr: the number
+ * Return: string
+ */
+char *int2hex(char length, void const * const ptr)
+{
+	char hexa[100];
+	int i, j, k;
+	unsigned long int n, temp;
+	char *str;
+
+	i = 1;
+	k = 0;
+	if (length == 'l')
+		n = *((long int *)ptr);
+	else if (length == 'h')
+		n = *((short int *)ptr);
+	else if (length == '\0')
+		n = *((int *)ptr);
+	while (n != 0)
+	{
+		temp = n % 16;
+		if (temp < 10)
+			temp = temp + 48;
+		else
+			temp = temp + 55;
+		hexa[i++] = temp;
+		n = n / 16;
+	}
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	for (j = i - 1; j > 0; j--, k++)
+	{
+		str[k] = hexa[j];
+	}
+	str[k] = '\0';
+	return (str);
+}
