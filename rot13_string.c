f11d89ddd9ba30543ca2307ebb407c87cc425030
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-07-25 03:50:32 +0300

    Rotates a string

---
+++ rot13_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - rotates the string by 13 places and integer by 5 places.
+ * @str: the required string
+ * Return: on success a string (str)
+ */
+char *rot13(char *str)
+{
+	unsigned int len, i;
+
+	len = str_len(str);
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 13;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] + 13;
+		else if (str[i] >= '0' && str[i] <= '9')
+			str[i] = str[i] + 5;
+	}
+	return (str);
+}
