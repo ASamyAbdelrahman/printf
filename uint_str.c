68dfef73993b0e18afae73bb5db5e5a138d493ca
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-07-25 16:46:01 +0300

    converts unsigned integer to string

--- uint_str.c
+++ uint_str.c
@@ -3,34 +3,47 @@
 
 /**
  * uint2str - converts unsigned int to string
- * @num: the required number
+ * @length: the required data type
+ * @ptr: the required number
+ * @put_positive_sign: the sign of the number
  * Return: on success a string (str)
  */
-char *uint2str(unsigned int num)
+char *uint2str(char length, void const * const ptr, char put_positive_sign)
 {
 	char *str;
-	unsigned int n, rem;
+	unsigned long int n, rem, num;
 	int len, i;
 
+	if (length == 'l')
+		num = *((unsigned long int *)ptr);
+	else if (length == 'h')
+		num = *((unsigned short int *)ptr);
+	else if (length == '\0')
+		num = *((unsigned int *)ptr);
 	n = num;
-	len = 0;
+	len = i = 0;
+	if (num == 0)
+	{
+		str = (char *)malloc(sizeof(char) * 2);
+		str[i++] = '0';
+		str[i] = '\0';
+		return (str);
+	}
 	while (n != 0)
 	{
 		len++;
 		n /= 10;
 	}
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-		return (NULL);
-	if (str != NULL)
+	str = (char *)malloc(sizeof(char) * (len + 2));
+	while (num != 0)
 	{
-		for (i = 0; i < len; i++)
-		{
-			rem = num % 10;
-			num = num / 10;
-			str[len - (i + 1)] = rem + '0';
-		}
+		rem = num % 10;
+		str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+		num = num / 10;
 	}
-	str[len] = '\0';
+	if (put_positive_sign == '1')
+		str[i++] = '+';
+	str[i] = '\0';
+	reverse(str);
 	return (str);
 }
